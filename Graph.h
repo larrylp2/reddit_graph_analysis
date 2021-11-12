658ea3205de39e780ba016c80b4be30607f1839d
Khanh Nguyen <khanhn2@illinois.edu>
2021-11-11 19:03:03 -0600

    Added Graph class

---
+++ Graph.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+class Graph {
+    struct Node
+    {
+        //Name of this subreddit
+        string name; 
+        //List of its adjacentNodes, map the name of the subreddit to a pair of Node* and long.
+        //long is the weight of the connection (user count) and Node* points to the other subreddit node.
+        map <string, pair<Node*, long>> adjacentNodes; 
+    };
+    
+    public:
+        Graph();
+        
+    private:
+        vector<Node> nodeList; //Store the list of all nodes
+};
