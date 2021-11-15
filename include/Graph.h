086c2c9c034d2b4e35ed8407acdad6138c27415a
Khanh Nguyen <khanhn2@illinois.edu>
2021-11-14 18:17:29 -0600

    Changed GraphBuilder to Graph

--- include/Graph.h
+++ include/Graph.h
@@ -1,23 +1,49 @@
 #pragma once
-#include <string>
-#include <map>
+
 #include <vector>
+#include <map>
+#include <fstream>
+#include <iostream>
 
-using namespace std;
+//#include "Graph.h"
 
-class Graph {
+const std::string SUBREDDIT_DATA_PATH = "khanhn2-mohanty7--larrylp2/data-fetching/data/subreddit_text/";
+const std::string USER_DATA_PATH = "khanhn2-mohanty7--larrylp2/data-fetching/data/subreddit_text/";
+const std::string JSON_SUFFIX = ".json";
+
+
+class GraphBuilder {
+
     public:
-        struct Node {
-            //Name of this subreddit
-            string name; 
-            //List of its adjacentNodes, map the name of the subreddit to a pair of Node* and long.
-            //long is the weight of the connection (user count) and Node* points to the other subreddit node.
-            map <string, pair<Node*, long> > adjacentNodes;
+        struct SubReddit {
+            std::string name;
+            std::vector<std::string> users;
+            std::vector<SubReddit*> adjacentSubReddits;
         };
 
-        Graph();
+        GraphBuilder();
 
+        /*
+         * Reads in from subreddit file usernames, adding those users to the current subreddit then calling populate subreddits on each user
+         *
+         */
+        void populateSubReddits(std::string file_name);
+
+        /*
+         * Reads in from a user file subreddit names, initializing new subreddits if necessary
+         * @param file_name the name of the inital file to be read in
+         */
+        //void PopulateSubreddits(std::string file_name);
+
+        /*
+         * Gets the SubReddit pointer for a subreddit name from the map of unique subreddits
+         * @param subReddit the name of the subreddit to find
+         * @return the pointer to the input subReddit if found, NULL if not found
+         */
+        SubReddit* getSubReddit(std::string subReddit);
 
     private:
-        map<string, Node*> nodeList; //Store the list of all nodes, map the name to the pointer
+        //create a map with pointers to subreddit nodes as values and subreddit names as keys
+        std::map<std::string, SubReddit*> unique_subreddits;
+
 };
