e303830a6064f3b435b4e3bee759002fe6b905f0
Khanh Nguyen <khanhn2@illinois.edu>
2021-11-28 17:56:46 -0600

    Put Graph.cpp and Graph.h outside

--- main.cpp
+++ main.cpp
@@ -12,17 +12,17 @@ int main() {
 
     std::string file_path = "data-fetching/data/subreddit_test/UIUC.txt";
 
-    builder.populateSubReddits(file_path);
+    // builder.populateSubReddits(file_path);
 
-    GraphBuilder::SubReddit* sub = builder.getSubReddit("UIUC");
+    // GraphBuilder::SubReddit* sub = builder.getSubReddit("UIUC");
 
     //iterate through the subreddit's adjacent nodes
     std::cout << "Adjacent SubReddits" << std::endl;
 
-    int size = sub->adjacentSubReddits.size();
-    for(int i = 0; i < size; i++) {
-        std::cout << sub->adjacentSubReddits[i]->name << std::endl;
-    }
+    // int size = sub->adjacentSubReddits.size();
+    // for(int i = 0; i < size; i++) {
+    //     std::cout << sub->adjacentSubReddits[i]->name << std::endl;
+    // }
 
     return 0;
 }
