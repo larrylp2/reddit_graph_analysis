25da36bf19462d2ce074bc2c291f727ea32168b0
larrylp2 <larrylp2@illinois.edu>
2021-11-28 17:19:07 -0600

    Remove unneeded code, adjust ordering

--- include/Graph.h
+++ include/Graph.h
@@ -9,7 +9,7 @@
 //#include "Graph.h"
 
 const string SUBREDDIT_DATA_PATH = "khanhn2-mohanty7--larrylp2/data-fetching/data/subreddit_text_new/";
-const string USER_DATA_PATH = "khanhn2-mohanty7--larrylp2/data-fetching/data/subreddit_text_new/";
+const string USER_DATA_PATH = "khanhn2-mohanty7--larrylp2/data-fetching/data/user_text_new/";
 const string JSON_SUFFIX = ".json";
 const string TXT_SUFFIX = ".txt";
 using namespace std;
@@ -26,37 +26,8 @@ class GraphBuilder {
         GraphBuilder();
         GraphBuilder(string source_directory); //Construct with the directory
 
-        /*
-         * Reads in from subreddit file usernames, adding those users to the current subreddit then calling populate subreddits on each user
-         *
-         */
-        void populateSubReddits(string file_name);
-
-        // More readable version of populateSubReddits and will replace that version after implemented
-        // Maybe put in private since this will be called in readGraph
-        void toBeImplementedPopulateSubreddit(string name); 
-
         // Read and construct the graph, which calls populateSubReddits to all subreddits
         void readGraph(string start);
-
-
-        //Helper function that retrieves the pointer to a subreddit based on its name, creating a new SubReddit struct if that subreddit does not already exist
-        SubReddit* retrieveSubreddit(string name);
-
-
-        //helper function that adds edges between two subreddits (assume that sub1 and sub2 already exist in unique_subreddits)
-        void connectSubreddits(SubReddit* sub1, SubReddit* sub2);
-
-        //helper function that adds edges between all subreddits in a vector
-        void connectSubreddits(vector<SubReddit*> subs);
-        
-
-        /*
-         * Reads in from a user file subreddit names, initializing new subreddits if necessary
-         * @param file_name the name of the inital file to be read in
-         */
-        //void PopulateSubreddits(string file_name);
- 
 
         void BFSTraversal() const; //Write to a txt file all the nodes in the graph+
 
@@ -67,19 +38,24 @@ class GraphBuilder {
         
         map<string, SubReddit*> unique_subreddits; //a map to keep track of subreddit object pointers (vertices)
 
-        map<pair<SubReddit*, SubReddit*>, int> edges; //a set to keep track of the list of edges and their strengths
+        //map<pair<SubReddit*, SubReddit*>, int> edges; //a set to keep track of the list of edges and their strengths
 
         set<string> checked_users; // A set to store users that have already been checked
 
         set<SubReddit*> read_subs; // A set to store subs that have already been stored
 
+        // More readable version of populateSubReddits and will replace that version after implemented
+        // Maybe put in private since this will be called in readGraph
+        void populateSubreddit(string name); 
 
-        // Add weight to the connection between two subreddits
-        // If a subreddit isn't exist yet, create new SubReddit* and add to unique_subreddits
-        void addWeight(string subreddit1, string subreddit2);
-        // List version of addWeight, which call addWeight to all pairs in the list
-        void addWeightToList(vector<string> subreddit_list); 
+        //Helper function that retrieves the pointer to a subreddit based on its name, creating a new SubReddit struct if that subreddit does not already exist
+        SubReddit* retrieveSubreddit(string name);
 
+        // List version of connectSubreddits, which calls connectSubreddits to all pairs in the list
+        void connectSubRedditList(vector<string> subreddit_list); 
+
+        //helper function that adds edges between two subreddits (assume that sub1 and sub2 already exist in unique_subreddits)
+        void connectSubreddits(SubReddit* sub1, SubReddit* sub2);
 
         // Search the file in source/subreddit_text and return the list
         // Return empty vector if not found
--- src/Graph.cpp
+++ src/Graph.cpp
@@ -9,6 +9,34 @@ GraphBuilder::GraphBuilder(string source_directory) {
     source = source_directory;
 }
 
+void GraphBuilder::readGraph(string root) {
+    // Create a new SubReddit of root and append to unique_subreddits
+    // Then call populate() to all the name in the unique_subreddits
+    // Be sure to not call it twice on one subreddit by making a map to keep track
+    populateSubreddit(root);
+    SubReddit* rootSub = unique_subreddits.find(root)->second;
+    read_subs.insert(rootSub);
+    for(map<SubReddit*, int>::iterator it = rootSub->adjacent.begin(); it != rootSub->adjacent.end(); it++) {
+        if(read_subs.find(it->first) != read_subs.end()) { //already read this subreddit 
+            //do nothing
+        } else {
+            //recursively read this next subreddit
+            readGraph(it->first->name);
+        }
+    }
+}
+
+void GraphBuilder::populateSubreddit(std::string name) {
+    // name is the name of the subreddit, example: UIUC
+
+    vector<string> user_list = getUserListFromSubRedditFile(name);
+    for (string u : user_list) {
+        if (checked_users.find(u) != checked_users.end()) { // If the user hasn't been checked
+            connectSubRedditList(getSubRedditListFromUserFile(u));
+        }
+    }
+}
+
 GraphBuilder::SubReddit* GraphBuilder::retrieveSubreddit(string subName) {
     //start by checking if the subreddit already exists in our map
     map<string, SubReddit*>::iterator iterator = unique_subreddits.find(subName);
@@ -38,103 +66,8 @@ void GraphBuilder::connectSubreddits(SubReddit* sub1, SubReddit* sub2) {
     }
 }
 
-void GraphBuilder::connectSubreddits(vector<SubReddit*> subs) {
-    for(int i = 0; i < subs.size(); i++) {
-        for(int j = i + 1; j < subs.size(); j++) {
-            connectSubreddits(subs[i], subs[j]); //simple algoritm to connect each subreddit with each other
-        }
-    }
-}
-
-void GraphBuilder::populateSubReddits(std::string file_name) {
-    //our file_name for this includes the name of the subreddit in question
-    string subReddit = file_name.substr(0, file_name.size() - 5); // the ".json" portion is size 5
-    
-    //the subreddit that our initial call was done from
-    SubReddit* original = retrieveSubreddit(subReddit);
-
-    ifstream sub_file(SUBREDDIT_DATA_PATH + file_name); //gets the relative path to the subreddit filename
-    //read it line by line
-    string user;
-    if(sub_file.is_open()) {
-        while(getline(sub_file, user)) {
-            //first line of the .txt file is '0'
-            if(user != "0") {
-                //check to see if this user has already been checked
-                if(checked_users.find(user) != checked_users.end()) {
-                    //this user has already been handled, do nothing
-                } else {
-                    //this user has not yet been handled, start process after adding this user to the list of already checked users
-                    checked_users.insert(user);
-                    original->users.insert(user); //add this user to the set of users seen active on this subreddit
-
-                    string user_file_name = user + TXT_SUFFIX;
-
-                    ifstream user_file(USER_DATA_PATH + user_file_name);
-                    string newSubReddit; //new subreddits that this user is active in
-                    vector<SubReddit*> subs; //keep track of all subreddits that this user is active in that is not our initial subreddit
-
-                    if(user_file.is_open()) {
-                        while(getline(user_file, newSubReddit)) {
-                            //first line of the .txt file is '0'
-                            if(newSubReddit != "0") {
-                                //only do stuff if this subReddit is not our starting subreddit
-                                if(newSubReddit != subReddit) {
-                                    
-                                    //get a pointer to the subreddit this user is active in
-                                    SubReddit* current = retrieveSubreddit(newSubReddit);
-
-                                    //update the adjacent subreddits
-                                    connectSubreddits(original, current);
-
-                                    subs.push_back(current);
-
-                                    //call populateSubReddits recursively on this new subreddit
-                                    populateSubReddits(newSubReddit + TXT_SUFFIX);
-                                }
-                            }
-                        }
-                    }
-
-                    //now connect all of the subreddits this user is active in to each other
-                    connectSubreddits(subs);   
-                }
-            }
-        }
-    }
-}
-
-
-void GraphBuilder::toBeImplementedPopulateSubreddit(std::string name) {
-    // name is the name of the subreddit, example: UIUC
-
-    vector<string> user_list = getUserListFromSubRedditFile(name);
-    for (string u : user_list) {
-        if (checked_users.find(u) != checked_users.end()) { // If the user hasn't been checked
-            addWeightToList(getSubRedditListFromUserFile(u));
-        }
-    }
-}
-
-void GraphBuilder::readGraph(string root) {
-    // Create a new SubReddit of root and append to unique_subreddits
-    // Then call toBeImplementedPopulateSubreddit() to all the name in the unique_subreddits
-    // Be sure to not call it twice on one subreddit by making a map to keep track
-    toBeImplementedPopulateSubreddit(root);
-    SubReddit* rootSub = unique_subreddits.find(root)->second;
-    read_subs.insert(rootSub);
-    for(map<SubReddit*, int>::iterator it = rootSub->adjacent.begin(); it != rootSub->adjacent.end(); it++) {
-        if(read_subs.find(it->first) != read_subs.end()) { //already read this subreddit 
-            //do nothing
-        } else {
-            //recursively read this next subreddit
-            readGraph(it->first->name);
-        }
-    }
-}
-
 // List version of addWeight, which call addWeight to all pairs in the list
-void GraphBuilder::addWeightToList(vector<string> subreddit_list) {
+void GraphBuilder::connectSubRedditList(vector<string> subreddit_list) {
     vector<SubReddit*> subs;
     for(int i = 0; i < subreddit_list.size(); i++) {
         subs.push_back(retrieveSubreddit(subreddit_list[i]));
