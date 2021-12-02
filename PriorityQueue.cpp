8db8598f476799b2fbfefb5886d89573a47b1dd9
Khanh Nguyen <khanhn2@illinois.edu>
2021-12-01 23:33:52 -0600

    Updated PriorityQueue

--- PriorityQueue.cpp
+++ PriorityQueue.cpp
@@ -1,1 +1,76 @@
 #include "PriorityQueue.h"
+
+PriorityQueue::PriorityQueue() {
+    // Default constructor
+    size = 0;
+    nodeList = map<Graph::SubReddit*, HeapNode*>();
+    root = NULL;
+}
+
+PriorityQueue::PriorityQueue(Graph::SubReddit* first, vector<Graph::SubReddit*> rest) {
+    // Default constructor
+}
+
+PriorityQueue::~PriorityQueue() {
+    // Destructor
+}
+
+Graph::SubReddit* PriorityQueue::peakMin() const {
+    return root->subreddit;
+}
+
+void PriorityQueue::popMin() {
+
+}
+
+void PriorityQueue::push(Graph::SubReddit* subreddit, double weight) {
+
+}
+
+void PriorityQueue::changeWeight(Graph::SubReddit* sub, double newWeight) {
+
+}
+bool PriorityQueue::isEmpty() const {
+
+}
+void PriorityQueue::heapifyUp(HeapNode* node) {
+    if (node == root) {
+        return;
+    }
+    HeapNode* p = node->parent;
+    if (lesser(node, p)) {
+        if (p->leftChild == node) {
+
+        } else {
+            
+        }
+        heapifyUp(p);
+    }
+}
+void PriorityQueue::heapifyDown(HeapNode* node) {
+
+}
+
+bool PriorityQueue::hasAChild(HeapNode* node) const{
+
+}
+
+bool PriorityQueue::lesser(HeapNode* left, HeapNode* right) const{
+    double wl = left->weight;
+    double wr = right->weight;
+    if (wl == -1) {
+        return false;
+    }
+    if (wr == -1) {
+        return true;
+    }
+    return (wl < wr);
+}
+
+PriorityQueue::HeapNode* PriorityQueue::rightMostNode() const {
+    
+}
+
+void PriorityQueue::swap(HeapNode* child, HeapNode* parent) {
+    
+}
--- PriorityQueue.h
+++ PriorityQueue.h
@@ -15,10 +15,22 @@ class PriorityQueue {
             HeapNode* rightChild;
         };
         PriorityQueue();
-        PriorityQueue(Graph::SubReddit* root, vector<Graph::SubReddit*> rest);
+        //Useful for initializing for dijkstra, where 1 node is initialy 0 and the rest is inf (-1)
+        PriorityQueue(Graph::SubReddit* first, vector<Graph::SubReddit*> rest); 
         ~PriorityQueue();
-
+        void push(Graph::SubReddit* subreddit, double weight);
+        Graph::SubReddit* peakMin() const;
+        void popMin();
+        void changeWeight(Graph::SubReddit* sub, double newWeight);
+        bool isEmpty() const;
     private:
         map<Graph::SubReddit*, HeapNode*> nodeList;
-
+        HeapNode* root;
+        int size;
+        void heapifyUp(HeapNode* node);
+        void heapifyDown(HeapNode* node);
+        void swap(HeapNode* child, HeapNode* parent);
+        bool hasAChild(HeapNode* node) const;
+        bool lesser(HeapNode* left, HeapNode* right) const;
+        HeapNode* rightMostNode() const;
 };
