11f081d0c68891ce387c11ffaa54928795c541a3
Khanh Nguyen <khanhn2@illinois.edu>
2021-12-05 14:38:57 -0600

    Fixed subreddit_data_path and user_data_path

--- Graph.h
+++ Graph.h
@@ -11,8 +11,8 @@ using namespace std;
 
 //const string SUBREDDIT_DATA_PATH = "khanhn2-mohanty7--larrylp2/data-fetching/data/subreddit_text_new/";
 //const string USER_DATA_PATH = "khanhn2-mohanty7--larrylp2/data-fetching/data/user_text_new/";
-const string SUBREDDIT_DATA_PATH = "subreddit_text_new/";
-const string USER_DATA_PATH = "user_text_new/";
+const string SUBREDDIT_DATA_PATH = "subreddit_text/";
+const string USER_DATA_PATH = "user_text/";
 const string JSON_SUFFIX = ".json";
 const string TXT_SUFFIX = ".txt";
 
