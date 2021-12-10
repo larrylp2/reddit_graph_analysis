961a437489b9a9330afe0ac4a2ddb6a6223b7fb4
larrylp2 <larrylp2@illinois.edu>
2021-12-10 00:50:20 -0600

    Adjust font file path

--- tests/testVisualizer.cpp
+++ tests/testVisualizer.cpp
@@ -163,7 +163,7 @@ TEST_CASE("Draw Edges Larger Graph", "[weight=1]")
     redditCoords.insert(pair<Graph::SubReddit*, pair<int, int>>(two, position4));
     redditCoords.insert(pair<Graph::SubReddit*, pair<int, int>>(three, position5));
 
-    GraphVisualization visual = GraphVisualization(4000, 4000, 4, "../InterFontCharacters/");
+    GraphVisualization visual = GraphVisualization(4000, 4000, 4, "khanhn2-mohanty7--larrylp2/InterFontCharacters/");
 
     cs225::PNG* drawing = visual.drawGraph(redditCoords);
     drawing->writeToFile("smallVisualizerTestOutput.png");
