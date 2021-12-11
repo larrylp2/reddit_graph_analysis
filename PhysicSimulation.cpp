15bdf7c635955f965d9144d83a1866a163bc91e4
Mohanty, Natasha <mohanty7@illinois.edu>
2021-12-10 19:27:53 -0500

    added simulateFor() method

--- PhysicSimulation.cpp
+++ PhysicSimulation.cpp
@@ -34,16 +34,54 @@ void PhysicSimulation::initiateGraph(Graph g) {
     
 }
 
-map<Graph::SubReddit*, pair<int, int>> PhysicSimulation::simulateFor(int seconds) {
+map<Graph::SubReddit*, pair<double, double>> PhysicSimulation::simulateFor(int seconds) {
+    //create a map that we will return
+    map<Graph::SubReddit*, pair<double, double>> new_positions;
+    
+    //loop through the amount of seconds
     for (int i = 0; i < seconds; i++) {
-        for (map<Graph::SubReddit*, pair<int, int>>::iterator it = positions.begin(); it != positions.end(); it++) {
-            std::pair<int, int> force_vector(0, 0);
-            //find distance between current node and adjacent node
-            // int x_distance = pow((it->second.first - it->first->adjacent.second), 2);
-            // int y_distance = pow((it->second.second - it->first->adjacent.second), 2);
 
-            // int total_distance = sqrt(x_distance + y_distance);
+        for (map<Graph::SubReddit*, pair<double, double>>::iterator it = positions.begin(); it != positions.end(); it++) {
+            
+            //finding individual coordinates for current node
+            double cX = it -> second.first;
+            double cY = it -> second.second;
+
+            //create our force vector
+            std::pair<double, double> force_vector(cX, cY);
+
+            //create a subreddit pointer for traversing adjacent nodes
+            Graph::SubReddit* subPtr = it -> first;
+
+            //loop through adjacent nodes map
+            for (map<Graph::SubReddit*, int>::iterator n = subPtr -> adjacent.begin(); n != subPtr -> adjacent.end(); n++) {
+                //need to find the position vector for the adjacent node
+                Graph::SubReddit* ptr = n -> first;
+
+                map<Graph::SubReddit*, pair<double, double>>::iterator iter = positions.find(ptr);
+                
+                //finding individual coordinates for adjacent node
+                double aX = iter -> second.first;
+                double aY = iter -> second.second;
+
+                //squaring the coordinate differences
+                double squared_x = pow(aX - cX, 2);
+                double squared_y = pow(aY - cY, 2);
+
+                //calculate the distance
+                double distance = sqrt(squared_x + squared_y);
+                //compute spring force vector formula
+
+                double spring_force_value = (distance - springNaturalLength) * springCoefficient;
+
+                //update the force vector
+                force_vector = make_pair(cX + spring_force_value, cY + spring_force_value);
+            }
+
+            //insert all the data into our newly created map
+            new_positions.insert(pair<Graph::SubReddit*, pair<double, double>>(it -> first, force_vector));
         }
     }
-    return map<Graph::SubReddit*, pair<int,int>>();
+
+    return new_positions;
 }
